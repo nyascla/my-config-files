f14a4d4d680a14c3cca51b8ca09f69d60bc372e6
Your Name <you@example.com>
2025-10-17 11:44:24 +0200

    eza

--- my-zsh.sh
+++ my-zsh.sh
@@ -113,12 +113,23 @@ install_xclip() {
     fi
 }
 
+install_eza() {
+    info "Instalando eza..."
+    if ! command -v eza &> /dev/null; then
+        sudo apt update && sudo apt install -y eza
+        info "eza instalado correctamente."
+    else
+        info "eza ya está instalado."
+    fi
+}
+
 main() {
     check_dependencies
     install_zsh
     install_xclip
     install_ohmyzsh
     install_fzf
+    install_eza
     copy_config_files
     configure_git
     info "¡Instalación completada! Por favor, reinicia tu terminal o inicia una nueva sesión de zsh."
